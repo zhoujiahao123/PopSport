186cfc62eceddc11d572f63db3fe61d13a3f5f88
zhoujiahao <zhou.jiahao@foxmail.com>
2017-01-18 17:06:54 +0800

    数据库基本建立

--- lib/src/main/java/com/example/MyClass.java
+++ lib/src/main/java/com/example/MyClass.java
@@ -5,16 +5,6 @@ import de.greenrobot.daogenerator.Entity;
 import de.greenrobot.daogenerator.Schema;
 
 public class MyClass {
-<<<<<<< HEAD
-<<<<<<< HEAD
-    public static void main(String []args) throws Exception{
-        Schema schema = new Schema(1,"com.nexuslink");
-        addUser(schema);
-        new DaoGenerator().generateAll(schema,"app/src/main/java-gen");
-=======
-=======
-
->>>>>>> a97d647881641ae8945a6cec92e58fd10ec526cf
     public static void main(String []args){
         Schema schema = new Schema(1,"com.nexuslink");
         createTable(schema);
@@ -24,10 +14,6 @@ public class MyClass {
             e.printStackTrace();
         }
 
-<<<<<<< HEAD
->>>>>>> db6c165b99a10fa2af1fab005d612dfc816881bb
-=======
->>>>>>> a97d647881641ae8945a6cec92e58fd10ec526cf
     }
     public static void createTable(Schema schema){
         Entity steps = schema.addEntity("Steps");
