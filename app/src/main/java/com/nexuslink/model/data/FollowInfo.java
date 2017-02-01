2c95ceb262e461f04b54cda656e58170cc403bc8
ZhangXingRui <2855892305@qq.com>
2017-02-01 10:29:48 +0800

    完善model层中缺少的flag标识位

--- app/src/main/java/com/nexuslink/model/data/FollowInfo.java
+++ app/src/main/java/com/nexuslink/model/data/FollowInfo.java
@@ -4,23 +4,27 @@ package com.nexuslink.model.data;
  * Created by ASUS-NB on 2017/1/22.
  */
 
-public class FollowInfo {
-    private int followFlag;
-    private int fFansNum;
+public class FollowInfo{
+    public static class FollowInfoBean {
+        private int followFlag;
+        private int fFansNum;
 
-    public int getFollowFlag() {
-        return followFlag;
-    }
+        public int getFollowFlag() {
+            return followFlag;
+        }
 
-    public void setFollowFlag(int followFlag) {
-        this.followFlag = followFlag;
-    }
+        public void setFollowFlag(int followFlag) {
+            this.followFlag = followFlag;
+        }
 
-    public int getfFansNum() {
-        return fFansNum;
-    }
+        public int getfFansNum() {
+            return fFansNum;
+        }
 
-    public void setfFansNum(int fFansNum) {
-        this.fFansNum = fFansNum;
+        public void setfFansNum(int fFansNum) {
+            this.fFansNum = fFansNum;
+        }
     }
 }
+
+
